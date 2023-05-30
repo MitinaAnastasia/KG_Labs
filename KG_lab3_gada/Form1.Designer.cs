b3e47af5203672cc81dc844204ce3d98d90e0ad8
Сысуева Анастасия <101638603+SysuevaAnastasia@users.noreply.github.com>
2023-05-30 16:19:57 +0300

    lab3

---
+++ KG_lab3_gada/Form1.Designer.cs
@@ -0,0 +1,166 @@
+﻿
+namespace KG_lab3_gada
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pan = new Tao.Platform.Windows.SimpleOpenGlControl();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.X = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Y = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Z = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Color = new System.Windows.Forms.DataGridViewButtonColumn();
+            this.Light = new System.Windows.Forms.Button();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.size = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pan
+            // 
+            this.pan.AccumBits = ((byte)(0));
+            this.pan.AutoCheckErrors = false;
+            this.pan.AutoFinish = false;
+            this.pan.AutoMakeCurrent = true;
+            this.pan.AutoSwapBuffers = true;
+            this.pan.BackColor = System.Drawing.Color.Black;
+            this.pan.ColorBits = ((byte)(32));
+            this.pan.DepthBits = ((byte)(16));
+            this.pan.Location = new System.Drawing.Point(12, 12);
+            this.pan.Name = "pan";
+            this.pan.Size = new System.Drawing.Size(528, 519);
+            this.pan.StencilBits = ((byte)(0));
+            this.pan.TabIndex = 0;
+            this.pan.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pan_MouseMove);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.X,
+            this.Y,
+            this.Z,
+            this.Color});
+            this.dataGridView1.Location = new System.Drawing.Point(546, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(383, 150);
+            this.dataGridView1.TabIndex = 1;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // X
+            // 
+            this.X.HeaderText = "X";
+            this.X.MinimumWidth = 6;
+            this.X.Name = "X";
+            this.X.Width = 80;
+            // 
+            // Y
+            // 
+            this.Y.HeaderText = "Y";
+            this.Y.MinimumWidth = 6;
+            this.Y.Name = "Y";
+            this.Y.Width = 80;
+            // 
+            // Z
+            // 
+            this.Z.HeaderText = "Z";
+            this.Z.MinimumWidth = 6;
+            this.Z.Name = "Z";
+            this.Z.Width = 80;
+            // 
+            // Color
+            // 
+            this.Color.HeaderText = "Color";
+            this.Color.MinimumWidth = 6;
+            this.Color.Name = "Color";
+            this.Color.Width = 80;
+            // 
+            // Light
+            // 
+            this.Light.Location = new System.Drawing.Point(677, 179);
+            this.Light.Name = "Light";
+            this.Light.Size = new System.Drawing.Size(125, 49);
+            this.Light.TabIndex = 2;
+            this.Light.Text = "Освещение";
+            this.Light.UseVisualStyleBackColor = true;
+            this.Light.Click += new System.EventHandler(this.Light_Click);
+            // 
+            // size
+            // 
+            this.size.Location = new System.Drawing.Point(549, 288);
+            this.size.Name = "size";
+            this.size.Size = new System.Drawing.Size(100, 22);
+            this.size.TabIndex = 3;
+            this.size.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(546, 257);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 17);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Размер:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(937, 546);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.size);
+            this.Controls.Add(this.Light);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.pan);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Tao.Platform.Windows.SimpleOpenGlControl pan;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn X;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Y;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Z;
+        private System.Windows.Forms.DataGridViewButtonColumn Color;
+        private System.Windows.Forms.Button Light;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.TextBox size;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ KG_lab3_gada/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Windows.Forms;
+using Tao.OpenGl;
+using Tao.FreeGlut;
+using System.Drawing;
+
+namespace KG_lab3_gada
+{
+    public partial class Form1 : Form
+    {
+        double _size = 10.0;
+        public Form1()
+        {
+            InitializeComponent();
+            pan.InitializeContexts();
+        }
+
+        float func(float x, float y)
+        {
+            return (float)(x*x - y*y);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        { 
+            Glut.glutInit();
+            Glut.glutInitDisplayMode(Glut.GLUT_RGB | Glut.GLUT_DOUBLE | Glut.GLUT_DEPTH);
+            Gl.glClearColor(0, 0, 0, 1);
+            Gl.glViewport(0, 0, pan.Width, pan.Height);
+            Gl.glMatrixMode(Gl.GL_PROJECTION);
+            Gl.glLoadIdentity();
+            const double W = 10;
+            double H = W * pan.Height / pan.Width;
+            Gl.glOrtho(-W, W, -H, H, -300, 300);
+            Gl.glMatrixMode(Gl.GL_MODELVIEW);
+            Gl.glLoadIdentity();
+            Gl.glEnable(Gl.GL_DEPTH_TEST);
+            Gl.glEnable(Gl.GL_COLOR_MATERIAL);
+            Gl.glEnable(Gl.GL_LIGHTING);
+            Gl.glEnable(Gl.GL_LIGHT0);
+        }
+
+        private void Render(double x, double y)
+        {
+            Gl.glClear(Gl.GL_COLOR_BUFFER_BIT | Gl.GL_DEPTH_BUFFER_BIT);
+
+            Gl.glLoadIdentity();
+            Gl.glColor3f(1, 1, 1);
+
+            Gl.glPushMatrix();
+            Gl.glTranslated(0, -1, -6);
+            Gl.glRotated((x - pan.Height) * 180 / pan.Height, 0, 1, 0);
+            Gl.glRotated((y - pan.Width) * 180 / pan.Width, 1, 0, 0);
+            Glut.glutSolidTeapot(_size);
+            Gl.glPopMatrix();
+            Gl.glFlush();
+            pan.Invalidate();
+        }
+
+        private void Light_Click(object sender, EventArgs e)
+        {
+            foreach (DataGridViewRow dgvr in dataGridView1.Rows)
+            {
+                if (dgvr.Index + 1 != dataGridView1.Rows.Count)
+                {
+                    Gl.glEnable(Gl.GL_LIGHT0 + dgvr.Index);
+                    float[] light_direction =
+                    {
+                            (float)Convert.ToDecimal(dgvr.Cells[0].Value),
+                            (float)Convert.ToDecimal(dgvr.Cells[1].Value),
+                            (float)Convert.ToDecimal(dgvr.Cells[2].Value),
+                            0
+                        };
+                    Gl.glLightfv(Gl.GL_LIGHT0 + dgvr.Index, Gl.GL_POSITION, light_direction);
+                    float[] diffuse_light =
+                    {
+                            ((Color)dgvr.Cells[3].Value).R / (float)255,
+                            ((Color)dgvr.Cells[3].Value).G / (float)255,
+                            ((Color)dgvr.Cells[3].Value).B / (float)255
+                        };
+                    Gl.glLightfv(Gl.GL_LIGHT0 + dgvr.Index, Gl.GL_DIFFUSE, diffuse_light);
+                }
+            }
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.ColumnIndex == 3)
+            {
+                if (DialogResult.OK == colorDialog1.ShowDialog())
+                {
+                    dataGridView1.Rows[e.RowIndex].Cells[3].Value = colorDialog1.Color;
+                }
+            }
+        }
+
+        private void pan_MouseMove(object sender, MouseEventArgs e)
+        {
+            Render(e.X, e.Y);
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            _size = Convert.ToDouble(size.Text);
+        }
+    }
+}
